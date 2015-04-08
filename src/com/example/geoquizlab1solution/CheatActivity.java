aa154f83249d8c8f4b5fa399f0767d8b600ef2f9
Bobby Rockers <bob@rockerssoft.org>
2015-04-07 21:57:01 -0500

    Task 3 of Lab 2.2

---
+++ src/com/example/geoquizlab1solution/CheatActivity.java
@@ -0,0 +1,17 @@
+package com.example.geoquizlab1solution;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheatActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_cheat);
+		
+	}
+
+}
